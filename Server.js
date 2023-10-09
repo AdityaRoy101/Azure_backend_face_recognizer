ffe8d2ddb0fc3557273ebf1804408a6443964cc7
AdityaDEVIL <2004003@kiit.ac.in>
2023-10-10 00:54:04 +0530

    All

--- Server.js
+++ Server.js
@@ -73,7 +73,7 @@ app.get('/profile/:id', (req,res) => { profile.handleProfile(req, res, db) })
 
 
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
-
+ 
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
 
